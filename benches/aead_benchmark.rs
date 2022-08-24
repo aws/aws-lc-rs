f52d2154be35d5830ccbd7e74bc301e31aa789d0
Justin Smith <justsmth@amazon.com>
2022-08-24 11:46:07 -0400

    Cleanup benchmarks

--- benches/aead_benchmark.rs
+++ benches/aead_benchmark.rs
@@ -1,8 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use hex::*;
-use paste::*;
 
 pub fn from_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     <Vec<u8>>::from_hex(hex_str).map_err(|e| String::from("Oops"))
@@ -37,6 +36,7 @@ macro_rules! benchmark_aead
 {
     paste::item! {
 mod [<$pkg _benchmarks>]  {
+    use criterion::black_box;
     use crate::AeadConfig;
     use $pkg::{aead, error, test};
     use aead::{
@@ -62,70 +62,59 @@ mod [<$pkg _benchmarks>]  {
         }
     }
 
-    impl crate::AeadConfig {
+    fn algorithm(config: &crate::AeadConfig) ->  &'static aead::Algorithm {
+        black_box(match &config.algorithm {
+            MY_AES_128_GCM => &aead::AES_128_GCM,
+            MY_AES_256_GCM => &aead::AES_256_GCM,
+            MY_CHACHA20_POLY1305 => &aead::CHACHA20_POLY1305,
+        })
+    }
 
-        fn [<algorithm_ $pkg>] (&self) -> &'static aead::Algorithm {
-            match &self.algorithm {
-                MY_AES_128_GCM => &aead::AES_128_GCM,
-                MY_AES_256_GCM => &aead::AES_256_GCM,
-                MY_CHACHA20_POLY1305 => &aead::CHACHA20_POLY1305,
-            }
-        }
+    fn key(config: &crate::AeadConfig) -> UnboundKey {
+       black_box(UnboundKey::new(algorithm(config), &config.key).unwrap())
+    }
 
-        fn [<key_ $pkg>] (&self) -> UnboundKey {
-            UnboundKey::new(self.[<algorithm_ $pkg>](), &self.key).unwrap()
-        }
-        fn [<aad_ $pkg>] (&self) -> Aad<String> {
-            Aad::from(self.aad.clone())
-        }
-        fn [<nonce_ $pkg>] (&self) -> impl NonceSequence {
-            //RngNonce{}
-            //NotANonce::new()
-            NotANonce::from(self.nonce.clone())
-        }
+    fn aad(config: &crate::AeadConfig) -> Aad<String> {
+        black_box(Aad::from(config.aad.clone()))
+    }
+
+    fn nonce(config: &crate::AeadConfig) -> impl NonceSequence {
+       black_box(NotANonce::from(config.nonce.clone()))
     }
 
     pub fn test_aead_separate_in_place( config: &AeadConfig, in_out: &mut Vec<u8> )  {
-        let mut sealing_key = SealingKey::new(config.[<key_ $pkg>](), config.[<nonce_ $pkg>]());
-        let mut opening_key = OpeningKey::new(config.[<key_ $pkg>](), config.[<nonce_ $pkg>]());
+        let mut sealing_key = SealingKey::new(key(&config), nonce(&config));
+        let mut opening_key = OpeningKey::new(key(&config), nonce(&config));
 
         let plaintext = in_out.clone();
         let tag = sealing_key
-            .seal_in_place_separate_tag(config.[<aad_ $pkg>](), in_out.as_mut_slice())
+            .seal_in_place_separate_tag(aad(&config), in_out.as_mut_slice())
             .map_err(|x| x.to_string()).unwrap();
 
         let cipher_text = in_out.clone();
-        if !plaintext.is_empty() {
-            assert_ne!(plaintext, cipher_text);
-        }
         let raw_tag = &tag as *const Tag as *const u8;
         let tag_value = unsafe { slice::from_raw_parts(raw_tag, 16) };
 
         in_out.extend(tag.as_ref());
         let result_plaintext = opening_key
-            .open_in_place(config.[<aad_ $pkg>](), in_out)
+            .open_in_place(aad(&config), in_out)
             .map_err(|x| x.to_string()).unwrap();
     }
 
     pub fn test_aead_append_within(config: &AeadConfig, in_out: &mut Vec<u8>) {
-        let mut sealing_key = SealingKey::new(config.[<key_ $pkg>](), config.[<nonce_ $pkg>]());
-        let mut opening_key = OpeningKey::new(config.[<key_ $pkg>](), config.[<nonce_ $pkg>]());
-
+        let mut sealing_key = SealingKey::new(key(&config), nonce(&config));
+        let mut opening_key = OpeningKey::new(key(&config), nonce(&config));
 
         let plaintext = in_out.clone();
         let mut sized_in_out = in_out.to_vec();
         sealing_key
-            .seal_in_place_append_tag(config.[<aad_ $pkg>](), &mut sized_in_out)
+            .seal_in_place_append_tag(aad(&config), &mut sized_in_out)
             .map_err(|x| x.to_string()).unwrap();
 
         let (cipher_text, tag_value) = sized_in_out.split_at_mut(plaintext.len());
 
-        if !plaintext.is_empty() {
-            assert_ne!(plaintext, cipher_text);
-        }
-
         let result_plaintext = opening_key
-            .open_within(config.[<aad_ $pkg>](), &mut sized_in_out, 0..)
+            .open_within(aad(&config), &mut sized_in_out, 0..)
             .map_err(|x| x.to_string()).unwrap();
     }
 
@@ -135,35 +124,50 @@ mod [<$pkg _benchmarks>]  {
 benchmark_aead!(ring);
 benchmark_aead!(aws_lc_ring_facade);
 
-fn test_aes_128_gcm(c: &mut Criterion) {
+fn test_aes_128_gcm_small_separate(c: &mut Criterion) {
     let config = AeadConfig::new(
         AeadAlgorithm::MY_AES_128_GCM,
         &from_hex("d480429666d48b400633921c5407d1d1").unwrap(),
         &from_hex("5bf11a0951f0bfc7ea5c9e58").unwrap(),
-        std::str::from_utf8(&from_hex("").unwrap()).unwrap(),
+        "aad value",
     );
-    let mut in_out = from_hex("").unwrap();
-    c.bench_function("ring-AES_128_GCM-separate: empty input", |b| {
+    let mut in_out = from_hex("0a2714aa7d").unwrap();
+    c.bench_function("ring-AES_128_GCM-separate: small input", |b| {
         b.iter(|| {
             ring_benchmarks::test_aead_separate_in_place(&config, &mut in_out);
         })
     });
-    c.bench_function("aws-lc-AES_128_GCM-separate: empty input", |b| {
+    c.bench_function("aws-lc-AES_128_GCM-separate: small input", |b| {
         b.iter(|| {
             aws_lc_ring_facade_benchmarks::test_aead_separate_in_place(&config, &mut in_out);
         })
     });
-    c.bench_function("ring-AES_128_GCM-append: empty input", |b| {
+}
+
+fn test_aes_128_gcm_small_append(c: &mut Criterion) {
+    let config = AeadConfig::new(
+        AeadAlgorithm::MY_AES_128_GCM,
+        &from_hex("d480429666d48b400633921c5407d1d1").unwrap(),
+        &from_hex("5bf11a0951f0bfc7ea5c9e58").unwrap(),
+        "aad value",
+    );
+    let mut in_out = from_hex("0a2714aa7d").unwrap();
+    c.bench_function("ring-AES_128_GCM-append: small input", |b| {
         b.iter(|| {
             ring_benchmarks::test_aead_append_within(&config, &mut in_out);
         })
     });
-    c.bench_function("aws-lc-AES_128_GCM-append: empty input", |b| {
+
+    c.bench_function("aws-lc-AES_128_GCM-append: small input", |b| {
         b.iter(|| {
             aws_lc_ring_facade_benchmarks::test_aead_append_within(&config, &mut in_out);
         })
     });
 }
 
-criterion_group!(benches, test_aes_128_gcm);
+criterion_group!(
+    benches,
+    test_aes_128_gcm_small_separate,
+    test_aes_128_gcm_small_append
+);
 criterion_main!(benches);
