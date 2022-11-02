8780e45732581ddf0211da9b682498ea3686f0a4
Sean McGrail <549813+skmcgrail@users.noreply.github.com>
2022-11-02 11:31:33 -0700

    Add test case for Okm to Salt bug (#81)

--- src/hkdf.rs
+++ src/hkdf.rs
@@ -382,4 +382,69 @@ mod tests {
         assert_eq!(ring_output1, output1);
         assert_eq!(ring_output2, output2);
     }
+
+    #[test]
+    fn okm_to_salt() {
+        const SALT: &[u8; 32] = &[
+            29, 113, 120, 243, 11, 202, 39, 222, 206, 81, 163, 184, 122, 153, 52, 192, 98, 195,
+            240, 32, 34, 19, 160, 128, 178, 111, 97, 232, 113, 101, 221, 143,
+        ];
+        const SECRET1: &[u8; 32] = &[
+            157, 191, 36, 107, 110, 131, 193, 6, 175, 226, 193, 3, 168, 133, 165, 181, 65, 120,
+            194, 152, 31, 92, 37, 191, 73, 222, 41, 112, 207, 236, 196, 174,
+        ];
+        const SECRET2: &[u8; 32] = &[
+            224, 63, 67, 213, 224, 104, 58, 50, 88, 209, 237, 46, 232, 170, 253, 41, 19, 19, 60,
+            235, 221, 215, 226, 154, 99, 234, 27, 43, 176, 174, 101, 21,
+        ];
+        const INFO1: &[&[u8]] = &[
+            &[
+                2, 130, 61, 83, 192, 248, 63, 60, 211, 73, 169, 66, 101, 160, 196, 212, 250, 113,
+            ],
+            &[
+                80, 46, 248, 123, 78, 204, 171, 178, 67, 204, 96, 27, 131, 24,
+            ],
+        ];
+        const INFO2: &[&[u8]] = &[
+            &[
+                34, 34, 23, 86, 156, 162, 231, 236, 148, 170, 84, 187, 88, 86, 15, 165, 95, 109,
+            ],
+            &[243, 251, 232, 90, 98, 26, 78, 75, 114, 115, 9, 72, 183, 193],
+        ];
+
+        let alg = HKDF_SHA256;
+        let salt = Salt::new(alg, SALT);
+        let prk = salt.extract(SECRET1);
+        let okm = prk.expand(INFO1, alg).unwrap();
+        let okm_salt: Salt = okm.into();
+        let prk2 = okm_salt.extract(SECRET2);
+        let okm2 = prk2.expand(INFO2, alg).unwrap();
+
+        let mut output = [0u8; 32];
+        okm2.fill(&mut output).expect("test failed");
+
+        println!("AWS-LC: {:?}", output);
+
+        let ring_alg = ring::hkdf::HKDF_SHA256;
+        let ring_salt = ring::hkdf::Salt::new(ring_alg, SALT);
+        let ring_prk = ring_salt.extract(SECRET1);
+        let ring_okm = ring_prk.expand(INFO1, ring_alg).unwrap();
+        let ring_okm_salt: ring::hkdf::Salt = ring_okm.into();
+        let ring_prk2 = ring_okm_salt.extract(SECRET2);
+        let ring_okm2 = ring_prk2.expand(INFO2, ring_alg).unwrap();
+
+        let mut ring_output = [0u8; 32];
+        ring_okm2.fill(&mut ring_output).expect("test failed");
+
+        println!("ring: {:?}", ring_output);
+
+        assert_eq!(ring_output, output);
+        assert_eq!(
+            output,
+            [
+                29, 148, 69, 177, 104, 16, 168, 31, 95, 217, 120, 105, 45, 141, 225, 36, 142, 230,
+                151, 143, 240, 12, 41, 129, 143, 119, 94, 221, 132, 167, 236, 243
+            ]
+        )
+    }
 }
