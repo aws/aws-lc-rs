f5447b94302486c96763f91ed45805efcfba4ff3
Justin Smith <justsmth@amazon.com>
2023-01-05 15:42:36 -0500

    Cleanup use of EVP_PKEY

--- src/ed25519.rs
+++ src/ed25519.rs
@@ -1,23 +1,19 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
-use crate::ec::PKCS8_DOCUMENT_MAX_LEN;
 use crate::error::{KeyRejected, Unspecified};
 use crate::pkcs8::Document;
-use crate::ptr::{ConstPointer, LcPtr};
+use crate::ptr::LcPtr;
 use crate::rand::SecureRandom;
 use crate::signature::{KeyPair, Signature, VerificationAlgorithm};
-use crate::{cbb, cbs, constant_time, sealed, test};
+use crate::{constant_time, sealed, test};
 use aws_lc::{
-    CBB_finish, ED25519_keypair_from_seed, ED25519_sign, ED25519_verify, EVP_PKEY_bits,
-    EVP_PKEY_get_raw_private_key, EVP_PKEY_get_raw_public_key, EVP_PKEY_id,
-    EVP_PKEY_new_raw_private_key, EVP_marshal_private_key, EVP_parse_private_key, EVP_PKEY,
-    EVP_PKEY_ED25519,
+    ED25519_keypair_from_seed, ED25519_sign, ED25519_verify, EVP_PKEY_get_raw_private_key,
+    EVP_PKEY_get_raw_public_key, EVP_PKEY_new_raw_private_key, EVP_PKEY, EVP_PKEY_ED25519,
 };
-use core::fmt;
+use std::fmt;
 use std::fmt::{Debug, Formatter};
 use std::mem::MaybeUninit;
-use std::os::raw::{c_int, c_uint};
 use std::ptr::null_mut;
 use untrusted::Input;
 
@@ -86,7 +82,7 @@ impl AsRef<[u8]> for PublicKey {
 }
 
 impl Debug for PublicKey {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.write_str(&format!("PublicKey(\"{}\")", test::to_hex(self.public_key)))
     }
 }
@@ -150,31 +146,7 @@ impl Ed25519KeyPair {
         unsafe {
             let evp_pkey = generate_key(rng)?;
 
-            let mut cbb = cbb::build_CBB(PKCS8_DOCUMENT_MAX_LEN);
-            if 1 != EVP_marshal_private_key(cbb.as_mut_ptr(), *evp_pkey) {
-                return Err(Unspecified);
-            }
-
-            let mut pkcs8_bytes_ptr = MaybeUninit::<*mut u8>::uninit();
-            let mut out_len = MaybeUninit::<usize>::uninit();
-            if 1 != CBB_finish(
-                cbb.as_mut_ptr(),
-                pkcs8_bytes_ptr.as_mut_ptr(),
-                out_len.as_mut_ptr(),
-            ) {
-                return Err(Unspecified);
-            }
-            let pkcs8_bytes_ptr = LcPtr::new(pkcs8_bytes_ptr.assume_init())?;
-            let out_len = out_len.assume_init();
-
-            let bytes_slice = std::slice::from_raw_parts(*pkcs8_bytes_ptr, out_len);
-            let mut pkcs8_bytes = [0u8; PKCS8_DOCUMENT_MAX_LEN];
-            pkcs8_bytes[0..out_len].copy_from_slice(bytes_slice);
-
-            Ok(Document {
-                bytes: pkcs8_bytes,
-                len: out_len,
-            })
+            evp_pkey.marshall_private_key()
         }
     }
 
@@ -250,12 +222,9 @@ impl Ed25519KeyPair {
     ///
     pub fn from_pkcs8_maybe_unchecked(pkcs8: &[u8]) -> Result<Self, KeyRejected> {
         unsafe {
-            let mut cbs = cbs::build_CBS(pkcs8);
+            let evp_pkey = LcPtr::try_from(pkcs8)?;
 
-            let evp_pkey = LcPtr::new(EVP_parse_private_key(&mut cbs))
-                .map_err(|_| KeyRejected::invalid_encoding())?;
-
-            validate_ed25519_evp_pkey(&evp_pkey.as_const())?;
+            evp_pkey.validate_as_ed25519()?;
 
             let mut private_key = [0u8; ED25519_PRIVATE_KEY_LEN];
             let mut out_len: usize = ED25519_PRIVATE_KEY_LEN;
@@ -307,31 +276,6 @@ impl Ed25519KeyPair {
             }))
         }
     }
-}
-
-#[inline]
-pub(crate) unsafe fn validate_ed25519_evp_pkey(
-    evp_pkey: &ConstPointer<EVP_PKEY>,
-) -> Result<(), KeyRejected> {
-    const ED25519_KEY_TYPE: c_int = EVP_PKEY_ED25519;
-    const ED25519_MIN_BITS: c_uint = 253;
-    const ED25519_MAX_BITS: c_uint = 256;
-
-    let key_type = EVP_PKEY_id(**evp_pkey);
-    if key_type != ED25519_KEY_TYPE {
-        return Err(KeyRejected::wrong_algorithm());
-    }
-
-    let bits = EVP_PKEY_bits(**evp_pkey);
-    let bits = c_uint::try_from(bits).map_err(|_| KeyRejected::unexpected_error())?;
-    if bits < ED25519_MIN_BITS {
-        return Err(KeyRejected::too_small());
-    }
-
-    if bits > ED25519_MAX_BITS {
-        return Err(KeyRejected::too_large());
-    }
-    Ok(())
 }
 
 #[cfg(test)]
