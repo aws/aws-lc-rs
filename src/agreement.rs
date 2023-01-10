7cd4da3e1d64179830872fb82536bc98d141d2c0
Justin Smith <justsmth@amazon.com>
2023-01-10 13:30:35 -0500

    Compute pubkey on request

--- src/agreement.rs
+++ src/agreement.rs
@@ -72,7 +72,6 @@ use aws_lc::{
     X25519_public_from_private, EC_KEY, NID_X25519,
 };
 use core::fmt;
-use std::cell::Cell;
 use std::fmt::{Debug, Formatter};
 use std::ptr::null_mut;
 use zeroize::Zeroize;
@@ -158,20 +157,13 @@ const X25519_SHARED_KEY_LEN: usize = aws_lc::X25519_SHARED_KEY_LEN as usize;
 enum KeyInner {
     ECDH_P256(LcPtr<*mut EC_KEY>),
     ECDH_P384(LcPtr<*mut EC_KEY>),
-    X25519(
-        [u8; X25519_PRIVATE_KEY_LEN],
-        Cell<Option<[u8; X25519_PUBLIC_VALUE_LEN]>>,
-    ),
+    X25519([u8; X25519_PRIVATE_KEY_LEN]),
 }
 
 impl Drop for KeyInner {
     fn drop(&mut self) {
-        if let KeyInner::X25519(private, public_cell) = self {
+        if let KeyInner::X25519(private) = self {
             private.zeroize();
-            let pub_key_option = public_cell.take();
-            if let Some(mut public_key) = pub_key_option {
-                public_key.zeroize();
-            }
         }
         // LcPtr's Drop implementation will call EC_KEY_free
     }
@@ -243,7 +235,7 @@ impl EphemeralPrivateKey {
 
     #[inline]
     fn from_x25519_private_key(priv_key: &[u8; X25519_PRIVATE_KEY_LEN]) -> Self {
-        let inner_key = KeyInner::X25519(*priv_key, Cell::new(None));
+        let inner_key = KeyInner::X25519(*priv_key);
         EphemeralPrivateKey { inner_key }
     }
 
@@ -293,18 +285,12 @@ impl EphemeralPrivateKey {
                     })
                 }
             }
-            KeyInner::X25519(priv_key, pub_key_cell) => {
-                if pub_key_cell.get().is_none() {
-                    let mut pub_key = [0u8; X25519_PUBLIC_VALUE_LEN];
-                    unsafe {
-                        X25519_public_from_private(pub_key.as_mut_ptr().cast(), priv_key.as_ptr());
-                    }
-                    pub_key_cell.set(Some(pub_key));
+            KeyInner::X25519(priv_key) => {
+                let mut buffer = [0u8; MAX_PUBLIC_KEY_LEN];
+                unsafe {
+                    X25519_public_from_private(buffer.as_mut_ptr().cast(), priv_key.as_ptr());
                 }
 
-                let mut buffer = [0u8; MAX_PUBLIC_KEY_LEN];
-                buffer[0..X25519_PUBLIC_VALUE_LEN]
-                    .copy_from_slice(&pub_key_cell.get().ok_or(Unspecified)?);
                 Ok(PublicKey {
                     alg: self.algorithm(),
                     public_key: buffer,
