481c7e80b0ae3953f9ff0569e1ec543ee1f4ec3b
Sean McGrail <mcgrails@amazon.com>
2023-02-22 19:55:50 -0800

    Use newer version of Go for docker images

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -ex
+#!/usr/bin/env bash
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0 OR ISC
+
+set -ex
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
 
 #############################################
 # Build images from aws-lc-rust GitHub repo #
@@ -14,11 +18,12 @@
 # Amazon Linux:
 # sudo yum install jq
 
-
 # Log Docker hub limit https://docs.docker.com/docker-hub/download-rate-limit/#how-can-i-check-my-current-rate
 TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
 curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
 
+pushd "${REPO_ROOT}/docker" &>/dev/null
 docker build -t rust:linux-386 linux-386 --load
 docker build -t rust:linux-arm64 linux-arm64 --load
 docker build -t rust:linux-x86_64 linux-x86_64 --load
+popd &>/dev/null
