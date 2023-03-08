b0e31435f0f7a26fa0ece4a914c4d69f5cfe48c5
Justin Smith <justsmth@amazon.com>
2023-03-08 11:03:51 -0500

    Fix clippy

--- aws-lc-fips-sys/builder/main.rs
+++ aws-lc-fips-sys/builder/main.rs
@@ -38,9 +38,7 @@ pub(crate) fn get_generated_include_path(manifest_dir: &Path) -> PathBuf {
 }
 
 pub(crate) fn get_aws_lc_fips_sys_includes_path() -> Option<PathBuf> {
-    env::var("AWS_LC_FIPS_SYS_INCLUDES")
-        .map(|path| PathBuf::from(path))
-        .ok()
+    env::var("AWS_LC_FIPS_SYS_INCLUDES").map(PathBuf::from).ok()
 }
 
 #[allow(dead_code)]
