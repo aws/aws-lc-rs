1b750bcc7abf8b1e70c6f2c52ff7f45c144bf20c
Sean McGrail <mcgrails@amazon.com>
2023-04-14 13:22:23 -0700

    Add semver script for determining latest aws-lc tag

---
+++ scripts/tools/semver.rs
@@ -0,0 +1,41 @@
+#!/usr/bin/env rust-script
+//! ```cargo
+//! [dependencies]
+//! clap = { version = "4.2.2", features = ["derive"] }
+//! regex = "1.7.3"
+//! semver = "1.0.17"
+//! ```
+
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(about)]
+struct Args {
+    tags: Vec<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let input: Vec<String> = args.tags;
+
+    // Modified regex from https://semver.org/
+    let re = regex::Regex::new(
+        r"^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$",
+    ).unwrap();
+
+    let mut tags: Vec<semver::Version> = input
+        .into_iter()
+        .filter(|t| re.is_match(t))
+        .map(|t| semver::Version::parse(t.strip_prefix("v").unwrap()).unwrap())
+        .collect();
+
+    tags.sort();
+
+    let latest = tags.pop().unwrap();
+
+    println!("v{latest}")
+}
