5a0fdf973225c906b319e9b0f39059964fc65327
Justin Smith <justsmth@amazon.com>
2023-05-16 17:00:25 -0400

    API changes POC

--- aws-lc-rs/src/cipher.rs
+++ aws-lc-rs/src/cipher.rs
@@ -3,6 +3,8 @@
 // Modifications copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
+#![allow(dead_code, unused_variables)]
+
 pub(crate) mod aes;
 pub(crate) mod block;
 pub(crate) mod chacha;
@@ -11,6 +13,8 @@ use crate::cipher::aes::{encrypt_block_aes, Aes128Key, Aes256Key};
 use crate::cipher::block::Block;
 use crate::cipher::chacha::ChaCha20Key;
 use crate::error::Unspecified;
+use crate::iv::IV;
+use crate::rand;
 use aws_lc::{AES_set_encrypt_key, AES_KEY};
 use std::mem::{size_of, transmute, MaybeUninit};
 use std::os::raw::c_uint;
@@ -38,6 +42,88 @@ impl Drop for SymmetricCipherKey {
             },
             SymmetricCipherKey::ChaCha20(_) => {}
         }
+    }
+}
+
+enum AlgorithmId {
+    Aes128ctr,
+    Aes128cbc,
+    Aes256ctr,
+    Aes256cbc,
+    Chacha20,
+}
+pub struct Algorithm<const KEYSIZE: usize, const IVSIZE: usize>(AlgorithmId);
+
+pub const AES128_CTR: Algorithm<16, 16> = Algorithm(AlgorithmId::Aes128ctr);
+pub const AES128_CBC: Algorithm<16, 16> = Algorithm(AlgorithmId::Aes128cbc);
+pub const AES256_CTR: Algorithm<32, 16> = Algorithm(AlgorithmId::Aes256ctr);
+pub const AES256_CBC: Algorithm<32, 16> = Algorithm(AlgorithmId::Aes256cbc);
+pub const CHACHA20: Algorithm<32, 12> = Algorithm(AlgorithmId::Chacha20);
+
+pub struct CipherKey<const KEYSIZE: usize, const IVSIZE: usize> {
+    algorithm: &'static Algorithm<KEYSIZE, IVSIZE>,
+    key: [u8; KEYSIZE],
+}
+
+impl<const KEYSIZE: usize, const IVSIZE: usize> CipherKey<KEYSIZE, IVSIZE> {
+    fn new(
+        algorithm: &'static Algorithm<KEYSIZE, IVSIZE>,
+        key_bytes: &[u8],
+    ) -> Result<Self, Unspecified> {
+        let key = key_bytes.try_into()?;
+        Ok(CipherKey { algorithm, key })
+    }
+}
+
+pub struct EncryptingKey<const KEYSIZE: usize, const IVSIZE: usize> {
+    cipher_key: CipherKey<KEYSIZE, IVSIZE>,
+    iv: IV<IVSIZE>,
+}
+
+impl<const KEYSIZE: usize, const IVSIZE: usize> EncryptingKey<KEYSIZE, IVSIZE> {
+    fn new(
+        cipher_key: CipherKey<KEYSIZE, IVSIZE>,
+    ) -> Result<EncryptingKey<KEYSIZE, IVSIZE>, Unspecified> {
+        let mut iv_bytes = [0u8; IVSIZE];
+        rand::fill(&mut iv_bytes)?;
+        Ok(EncryptingKey {
+            cipher_key,
+            iv: IV::assume_unique_for_key(iv_bytes),
+        })
+    }
+
+    #[must_use]
+    fn encrypt_in_place(self, in_out: &mut [u8]) -> Result<IV<IVSIZE>, Unspecified> {
+        // TODO: THIS IS A PROOF OF CONCEPT
+        // do nothing
+        Ok(self.iv)
+    }
+
+    fn encrypt_append_padding<INOUT>(self, in_out: INOUT) -> Result<IV<IVSIZE>, Unspecified>
+    where
+        INOUT: AsMut<[u8]> + for<'in_out> Extend<&'in_out u8>,
+    {
+        todo!()
+    }
+}
+
+pub struct DecryptingKey<const KEYSIZE: usize, const IVSIZE: usize> {
+    cipher_key: CipherKey<KEYSIZE, IVSIZE>,
+    iv: IV<IVSIZE>,
+}
+
+impl<const KEYSIZE: usize, const IVSIZE: usize> DecryptingKey<KEYSIZE, IVSIZE> {
+    fn new(
+        cipher_key: CipherKey<KEYSIZE, IVSIZE>,
+        iv: IV<IVSIZE>,
+    ) -> DecryptingKey<KEYSIZE, IVSIZE> {
+        DecryptingKey { cipher_key, iv }
+    }
+
+    fn decrypt_in_place(&self, in_out: &mut [u8]) -> Result<(), Unspecified> {
+        // TODO: THIS IS A PROOF OF CONCEPT
+        // do nothing
+        Ok(())
     }
 }
 
@@ -156,5 +242,40 @@ mod tests {
         let result = aes128.encrypt_block(Block::from(&input_block));
 
         assert_eq!(expected_result.as_slice(), result.as_ref());
+    }
+
+    #[test]
+    fn test_aes_128_ctr() {
+        let key = from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
+        let mut input = from_hex("00112233445566778899aabbccddeeff").unwrap();
+
+        let cipher_key = CipherKey::new(&AES128_CTR, &key).unwrap();
+        let encrypting_key = EncryptingKey::new(cipher_key).unwrap();
+
+        let mut inout = input.as_mut_slice();
+
+        let decrypt_iv = encrypting_key.encrypt_in_place(inout).unwrap();
+
+        let cipher_key2 = CipherKey::new(&AES128_CTR, &key).unwrap();
+        let decrypting_key = DecryptingKey::new(cipher_key2, decrypt_iv);
+
+        decrypting_key.decrypt_in_place(inout);
+    }
+
+    #[test]
+    fn test_aes_128_cbc() {
+        let key = from_hex("000102030405060708090a0b0c0d0e0f").unwrap();
+        let mut input = from_hex("00112233445566778899aabbccddeeff").unwrap();
+
+        let cipher_key = CipherKey::new(&AES128_CBC, &key).unwrap();
+        let encrypting_key = EncryptingKey::new(cipher_key).unwrap();
+
+        let mut inout = input.as_mut_slice();
+        let decrypt_iv = encrypting_key.encrypt_in_place(inout).unwrap();
+
+        let cipher_key2 = CipherKey::new(&AES128_CBC, &key).unwrap();
+        let decrypting_key = DecryptingKey::new(cipher_key2, decrypt_iv);
+
+        decrypting_key.decrypt_in_place(inout);
     }
 }
