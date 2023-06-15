4610af7b4be71faddfd54577dd3d08d5802f2e19
Justin Smith <justsmth@amazon.com>
2023-06-15 13:33:09 -0400

    Avoid additional unsafe blocks

--- aws-lc-rs/src/aead/aead_ctx.rs
+++ aws-lc-rs/src/aead/aead_ctx.rs
@@ -6,10 +6,9 @@ use crate::cipher::chacha;
 
 use crate::cipher::aes::{AES_128_KEY_LEN, AES_256_KEY_LEN};
 use crate::error::Unspecified;
-use crate::ptr::ConstPointer;
 use aws_lc::{
     EVP_AEAD_CTX_cleanup, EVP_AEAD_CTX_init, EVP_AEAD_CTX_zero, EVP_aead_aes_128_gcm,
-    EVP_aead_aes_256_gcm, EVP_aead_chacha20_poly1305, EVP_AEAD, EVP_AEAD_CTX,
+    EVP_aead_aes_256_gcm, EVP_aead_chacha20_poly1305, EVP_AEAD_CTX,
 };
 use std::mem::MaybeUninit;
 use std::ptr::null_mut;
@@ -33,55 +32,52 @@ impl AeadCtx {
         if AES_128_KEY_LEN != key_bytes.len() {
             return Err(Unspecified);
         }
-        unsafe {
-            let aead = ConstPointer::new(EVP_aead_aes_128_gcm())?;
-            Ok(AeadCtx::AES_128_GCM(AeadCtx::build_context(
-                &aead, key_bytes,
-            )?))
-        }
+        Ok(AeadCtx::AES_128_GCM(AeadCtx::build_context(
+            EVP_aead_aes_128_gcm,
+            key_bytes,
+        )?))
     }
 
     pub(crate) fn aes_256_gcm(key_bytes: &[u8]) -> Result<Self, Unspecified> {
         if AES_256_KEY_LEN != key_bytes.len() {
             return Err(Unspecified);
         }
-        unsafe {
-            let aead = ConstPointer::new(EVP_aead_aes_256_gcm())?;
-            Ok(AeadCtx::AES_256_GCM(AeadCtx::build_context(
-                &aead, key_bytes,
-            )?))
-        }
+        Ok(AeadCtx::AES_256_GCM(AeadCtx::build_context(
+            EVP_aead_aes_256_gcm,
+            key_bytes,
+        )?))
     }
 
     pub(crate) fn chacha20(key_bytes: &[u8]) -> Result<Self, Unspecified> {
         if chacha::KEY_LEN != key_bytes.len() {
             return Err(Unspecified);
         }
-        unsafe {
-            let aead = ConstPointer::new(EVP_aead_chacha20_poly1305())?;
-            Ok(AeadCtx::CHACHA20_POLY1305(AeadCtx::build_context(
-                &aead, key_bytes,
-            )?))
-        }
+        Ok(AeadCtx::CHACHA20_POLY1305(AeadCtx::build_context(
+            EVP_aead_chacha20_poly1305,
+            key_bytes,
+        )?))
     }
 
-    unsafe fn build_context(
-        aead: &ConstPointer<EVP_AEAD>,
+    fn build_context(
+        aead_fn: unsafe extern "C" fn() -> *const aws_lc::evp_aead_st,
         key_bytes: &[u8],
     ) -> Result<EVP_AEAD_CTX, Unspecified> {
         let mut aead_ctx = MaybeUninit::<EVP_AEAD_CTX>::uninit();
+        unsafe {
+            let aead = aead_fn();
 
-        if 1 != EVP_AEAD_CTX_init(
-            aead_ctx.as_mut_ptr(),
-            **aead,
-            key_bytes.as_ptr().cast(),
-            key_bytes.len(),
-            TAG_LEN,
-            null_mut(),
-        ) {
-            return Err(Unspecified);
+            if 1 != EVP_AEAD_CTX_init(
+                aead_ctx.as_mut_ptr(),
+                aead,
+                key_bytes.as_ptr().cast(),
+                key_bytes.len(),
+                TAG_LEN,
+                null_mut(),
+            ) {
+                return Err(Unspecified);
+            }
+            Ok(aead_ctx.assume_init())
         }
-        Ok(aead_ctx.assume_init())
     }
 }
 
