973840b7ab7c419ef4d5fe496951a7de8b43e484
Ben Civjan <bcivjan@amazon.com>
2023-07-13 13:03:34 -0700

    Add secp256k1 static variables with relevent digests

--- aws-lc-rs/src/signature.rs
+++ aws-lc-rs/src/signature.rs
@@ -537,6 +537,20 @@ pub static ECDSA_P521_SHA3_512_FIXED: EcdsaVerificationAlgorithm = EcdsaVerifica
     sig_format: EcdsaSignatureFormat::Fixed,
 };
 
+pub static ECDSA_P256K1_SHA256_FIXED: EcdsaVerificationAlgorithm = EcdsaVerificationAlgorithm {
+    id: &ec::AlgorithmID::ECDSA_P256K1,
+    digest: &digest::SHA256,
+    bits: 256,
+    sig_format: EcdsaSignatureFormat::Fixed,
+};
+
+pub static ECDSA_P256K1_SHA3_256_FIXED: EcdsaVerificationAlgorithm = EcdsaVerificationAlgorithm {
+    id: &ec::AlgorithmID::ECDSA_P256K1,
+    digest: &digest::SHA3_256,
+    bits: 256,
+    sig_format: EcdsaSignatureFormat::Fixed,
+};
+
 /// Verification of ASN.1 DER-encoded ECDSA signatures using the P-256 curve and SHA-256.
 pub static ECDSA_P256_SHA256_ASN1: EcdsaVerificationAlgorithm = EcdsaVerificationAlgorithm {
     id: &ec::AlgorithmID::ECDSA_P256,
@@ -593,6 +607,20 @@ pub static ECDSA_P521_SHA3_512_ASN1: EcdsaVerificationAlgorithm = EcdsaVerificat
     sig_format: EcdsaSignatureFormat::ASN1,
 };
 
+pub static ECDSA_P256K1_SHA256_ASN1: EcdsaVerificationAlgorithm = EcdsaVerificationAlgorithm {
+    id: &ec::AlgorithmID::ECDSA_P256K1,
+    digest: &digest::SHA256,
+    bits: 256,
+    sig_format: EcdsaSignatureFormat::ASN1,
+};
+
+pub static ECDSA_P256K1_SHA3_256_ASN1: EcdsaVerificationAlgorithm = EcdsaVerificationAlgorithm {
+    id: &ec::AlgorithmID::ECDSA_P256K1,
+    digest: &digest::SHA3_256,
+    bits: 256,
+    sig_format: EcdsaSignatureFormat::ASN1,
+};
+
 /// Signing of fixed-length (PKCS#11 style) ECDSA signatures using the P-256 curve and SHA-256.
 pub static ECDSA_P256_SHA256_FIXED_SIGNING: EcdsaSigningAlgorithm =
     EcdsaSigningAlgorithm(&ECDSA_P256_SHA256_FIXED);
@@ -613,6 +641,12 @@ pub static ECDSA_P521_SHA512_FIXED_SIGNING: EcdsaSigningAlgorithm =
 pub static ECDSA_P521_SHA3_512_FIXED_SIGNING: EcdsaSigningAlgorithm =
     EcdsaSigningAlgorithm(&ECDSA_P521_SHA3_512_FIXED);
 
+pub static ECDSA_P256K1_SHA256_FIXED_SIGNING: EcdsaSigningAlgorithm =
+    EcdsaSigningAlgorithm(&ECDSA_P256K1_SHA256_FIXED);
+
+pub static ECDSA_P256K1_SHA3_256_FIXED_SIGNING: EcdsaSigningAlgorithm =
+    EcdsaSigningAlgorithm(&ECDSA_P256K1_SHA3_256_FIXED);
+
 /// Signing of ASN.1 DER-encoded ECDSA signatures using the P-256 curve and SHA-256.
 pub static ECDSA_P256_SHA256_ASN1_SIGNING: EcdsaSigningAlgorithm =
     EcdsaSigningAlgorithm(&ECDSA_P256_SHA256_ASN1);
@@ -632,6 +666,12 @@ pub static ECDSA_P521_SHA512_ASN1_SIGNING: EcdsaSigningAlgorithm =
 /// Signing of ASN.1 DER-encoded ECDSA signatures using the P-521 curve and SHA3-512.
 pub static ECDSA_P521_SHA3_512_ASN1_SIGNING: EcdsaSigningAlgorithm =
     EcdsaSigningAlgorithm(&ECDSA_P521_SHA3_512_ASN1);
+
+pub static ECDSA_P256K1_SHA256_ASN1_SIGNING: EcdsaSigningAlgorithm =
+    EcdsaSigningAlgorithm(&ECDSA_P256K1_SHA256_ASN1);
+
+pub static ECDSA_P256K1_SHA3_256_ASN1_SIGNING: EcdsaSigningAlgorithm =
+    EcdsaSigningAlgorithm(&ECDSA_P256K1_SHA3_256_ASN1);
 
 /// Verification of Ed25519 signatures.
 pub static ED25519: EdDSAParameters = EdDSAParameters {};
