5e2c29201a06dec0000f7386ab4f0754e69f0ef9
Ben Civjan <bencivjan@gmail.com>
2023-07-14 17:45:11 -0500

    Create benchmarking for KEM API - Kyber512 (#185)

---
+++ aws-lc-rs-testing/benches/kem_benchmark.rs
@@ -0,0 +1,137 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
+use aws_lc_rs::{test, test_file};
+use criterion::{criterion_group, criterion_main, Criterion};
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum Algorithm {
+    KYBER512_R3,
+}
+
+impl From<&str> for Algorithm {
+    fn from(value: &str) -> Self {
+        match value {
+            "KYBER512_R3" => Algorithm::KYBER512_R3,
+            _ => panic!("Unrecognized algorithm: '{value}'"),
+        }
+    }
+}
+
+pub struct KemConfig {
+    algorithm: Algorithm,
+    public_key: Box<[u8]>,
+    secret_key: Box<[u8]>,
+    ciphertext: Box<[u8]>,
+}
+
+impl KemConfig {
+    fn new(algorithm: &str, public_key: &[u8], secret_key: &[u8], ciphertext: &[u8]) -> Self {
+        KemConfig {
+            algorithm: Algorithm::from(algorithm),
+            public_key: Vec::from(public_key).into(),
+            secret_key: Vec::from(secret_key).into(),
+            ciphertext: Vec::from(ciphertext).into(),
+        }
+    }
+}
+
+mod aws_lc_rs_benchmarks {
+
+    use aws_lc_rs::kem;
+
+    use crate::{Algorithm, KemConfig};
+    use kem::{KemAlgorithm, KemPrivateKey, KemPublicKey, KYBER512_R3};
+
+    fn algorithm(config: &KemConfig) -> &'static KemAlgorithm {
+        match config.algorithm {
+            Algorithm::KYBER512_R3 => &KYBER512_R3,
+        }
+    }
+
+    pub fn new_private_key(config: &KemConfig) -> KemPrivateKey {
+        KemPrivateKey::new(algorithm(config), &config.secret_key).unwrap()
+    }
+
+    pub fn new_public_key(config: &KemConfig) -> KemPublicKey {
+        KemPublicKey::new(algorithm(config), &config.public_key).unwrap()
+    }
+
+    pub fn keygen(config: &KemConfig) {
+        let private_key = KemPrivateKey::generate(algorithm(config)).unwrap();
+        let _public_key = private_key.compute_public_key().unwrap();
+    }
+
+    pub fn encapsulate(public_key: &KemPublicKey) {
+        public_key
+            .encapsulate((), |ct, ss| Ok((Vec::from(ct), Vec::from(ss))))
+            .unwrap();
+    }
+
+    pub fn decapsulate(config: &KemConfig, secret_key: &KemPrivateKey) {
+        let mut ciphertext = config.ciphertext.clone();
+        secret_key
+            .decapsulate(&mut ciphertext, (), |ss| Ok(Vec::from(ss)))
+            .unwrap();
+    }
+}
+
+fn bench_kem_keygen(c: &mut Criterion, config: &KemConfig) {
+    let bench_group_name = format!("KEM-{:?}-keygen", config.algorithm);
+
+    let mut group = c.benchmark_group(bench_group_name);
+
+    group.bench_function("AWS-LC", |b| {
+        b.iter(|| {
+            aws_lc_rs_benchmarks::keygen(config);
+        });
+    });
+}
+
+fn bench_kem_encapsulate(c: &mut Criterion, config: &KemConfig) {
+    let bench_group_name = format!("KEM-{:?}-encapsulate", config.algorithm);
+
+    let mut group = c.benchmark_group(bench_group_name);
+
+    group.bench_function("AWS-LC", |b| {
+        b.iter(|| {
+            let public_key = aws_lc_rs_benchmarks::new_public_key(config);
+            aws_lc_rs_benchmarks::encapsulate(&public_key);
+        });
+    });
+}
+
+fn bench_kem_decapsulate(c: &mut Criterion, config: &KemConfig) {
+    let bench_group_name = format!("KEM-{:?}-decapsulate", config.algorithm);
+
+    let mut group = c.benchmark_group(bench_group_name);
+
+    group.bench_function("AWS-LC", |b| {
+        b.iter(|| {
+            let private_key = aws_lc_rs_benchmarks::new_private_key(config);
+            aws_lc_rs_benchmarks::decapsulate(config, &private_key);
+        });
+    });
+}
+
+fn bench_kem(c: &mut Criterion) {
+    test::run(
+        test_file!("data/kem_benchmarks.txt"),
+        |_section, test_case| {
+            let config = KemConfig::new(
+                test_case.consume_string("algorithm").as_str(),
+                test_case.consume_bytes("pk").as_slice(),
+                test_case.consume_bytes("sk").as_slice(),
+                test_case.consume_bytes("ct").as_slice(),
+            );
+            bench_kem_keygen(c, &config);
+            bench_kem_encapsulate(c, &config);
+            bench_kem_decapsulate(c, &config);
+            Ok(())
+        },
+    );
+}
+
+criterion_group!(benches, bench_kem);
+criterion_main!(benches);
---
+++ aws-lc-rs-testing/tests/quic_test.rs
@@ -0,0 +1,33 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
+use aws_lc_rs::aead::quic::{HeaderProtectionKey, AES_128, AES_256, CHACHA20};
+use aws_lc_rs::{hkdf, test};
+
+#[test]
+fn test_key_type_header_protection_key() {
+    let key_bytes = test::from_dirty_hex(r#"d480429666d48b400633921c5407d1d1"#);
+    let info = test::from_dirty_hex(r#"f0f1f2f3f4f5f6f7f8f9"#);
+    let info_wrap = [info.as_slice()];
+    let sample = test::from_dirty_hex(r#"b0b1b2b3b4b5b6b7b8b9babbbcbdbebf"#);
+
+    let prk = hkdf::Prk::new_less_safe(hkdf::HKDF_SHA256, &key_bytes);
+    let okm = prk.expand(&info_wrap, &AES_128).unwrap();
+    let hpk = HeaderProtectionKey::from(okm);
+    assert_eq!(&AES_128, hpk.algorithm());
+    assert_eq!(16, AES_128.key_len());
+    assert_eq!(16, AES_128.sample_len());
+    assert_eq!(32, AES_256.key_len());
+    assert_eq!(16, AES_256.sample_len());
+    assert_eq!(32, CHACHA20.key_len());
+    assert_eq!(16, CHACHA20.sample_len());
+    let mask = hpk.new_mask(&sample).unwrap();
+
+    let ring_prk = ring::hkdf::Prk::new_less_safe(ring::hkdf::HKDF_SHA256, &key_bytes);
+    let ring_okm = ring_prk
+        .expand(&info_wrap, &ring::aead::quic::AES_128)
+        .unwrap();
+    let ring_hpk = ring::aead::quic::HeaderProtectionKey::from(ring_okm);
+    let ring_mask = ring_hpk.new_mask(&sample).unwrap();
+    assert_eq!(mask, ring_mask);
+}
