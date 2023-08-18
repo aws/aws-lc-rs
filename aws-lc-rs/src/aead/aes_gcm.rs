c82fbd18d192b5ed6b045c42f73adff85096402a
Anton Romanov <romanton@amazon.com>
2023-08-18 22:49:52 +0000

    Make defined algorithms const instead of static
    There seem to be no reason for them to be static

--- aws-lc-rs/src/aead/aes_gcm.rs
+++ aws-lc-rs/src/aead/aes_gcm.rs
@@ -54,7 +54,7 @@ pub(crate) fn aead_seal_separate(
 }
 
 /// AES-128 in GCM mode with 128-bit tags and 96 bit nonces.
-pub static AES_128_GCM: Algorithm = Algorithm {
+pub const AES_128_GCM: Algorithm = Algorithm {
     init: init_128_aead,
     key_len: AES_128_KEY_LEN,
     id: AlgorithmID::AES_128_GCM,
@@ -62,7 +62,7 @@ pub static AES_128_GCM: Algorithm = Algorithm {
 };
 
 /// AES-256 in GCM mode with 128-bit tags and 96 bit nonces.
-pub static AES_256_GCM: Algorithm = Algorithm {
+pub const AES_256_GCM: Algorithm = Algorithm {
     init: init_256_aead,
     key_len: AES_256_KEY_LEN,
     id: AlgorithmID::AES_256_GCM,
@@ -70,7 +70,7 @@ pub static AES_256_GCM: Algorithm = Algorithm {
 };
 
 /// AES-256 in GCM mode with nonce reuse resistance, 128-bit tags and 96 bit nonces.
-pub static AES_256_GCM_SIV: Algorithm = Algorithm {
+pub const AES_256_GCM_SIV: Algorithm = Algorithm {
     init: init_256_aead_siv,
     key_len: AES_256_KEY_LEN,
     id: AlgorithmID::AES_256_GCM_SIV,
@@ -78,7 +78,7 @@ pub static AES_256_GCM_SIV: Algorithm = Algorithm {
 };
 
 /// AES-128 in GCM mode with nonce reuse resistance, 128-bit tags and 96 bit nonces.
-pub static AES_128_GCM_SIV: Algorithm = Algorithm {
+pub const AES_128_GCM_SIV: Algorithm = Algorithm {
     init: init_128_aead_siv,
     key_len: AES_128_KEY_LEN,
     id: AlgorithmID::AES_128_GCM_SIV,
