e9c305b79ecb3a653e683a568fed04f27b3a40c3
Cameron Bytheway <bytheway.cameron@gmail.com>
2023-11-15 12:11:07 -0700

    use toml_edit to read expected links value

--- aws-lc-sys-testing/build.rs
+++ aws-lc-sys-testing/build.rs
@@ -1,15 +1,22 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
+use toml_edit::Document;
+
 fn main() {
+    let cargo_toml = std::fs::read_to_string("../aws-lc-sys/Cargo.toml").unwrap();
+    let cargo_toml = cargo_toml.parse::<Document>().unwrap();
+
+    let links = cargo_toml["package"]["links"].as_str().unwrap();
+
     // ensure that the include path is exported and set up correctly
     cc::Build::new()
-        .include(env("DEP_AWS_LC_0_12_0_INCLUDE"))
+        .include(env(format!("DEP_{}_INCLUDE", links.to_uppercase())))
         .file("src/testing.c")
         .compile("aws_ls_sys_testing");
 
     // ensure the libcrypto artifact is linked
-    println!("cargo:rustc-link-lib=aws_lc_0_12_0_crypto");
+    println!("cargo:rustc-link-lib={}_crypto", links);
 }
 
 fn env<S: AsRef<str>>(s: S) -> String {
