210a041791319cae8f7ce2f9ffd74612f297102c
Cameron Bytheway <bytheway.cameron@gmail.com>
2023-11-15 11:14:05 -0700

    fix failing ci tasks

--- aws-lc-sys-testing/build.rs
+++ aws-lc-sys-testing/build.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
 fn main() {
     // ensure that the include path is exported and set up correctly
     cc::Build::new()
--- aws-lc-sys-testing/src/main.rs
+++ aws-lc-sys-testing/src/main.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
 use std::ffi::c_int;
 
 extern "C" {
