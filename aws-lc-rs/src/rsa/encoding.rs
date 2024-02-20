b33bf02958ed0a9baec8158df23238d1d576a4e4
Sean McGrail <mcgrails@amazon.com>
2024-02-20 02:34:49 +0000

    Initial feedback pass

--- aws-lc-rs/src/rsa/encoding.rs
+++ aws-lc-rs/src/rsa/encoding.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR ISC
 
 /// PKCS#8 Encoding Functions
-pub(in super::super) mod pkcs8 {
+pub(in crate::rsa) mod pkcs8 {
     use crate::{
         cbb::LcCBB,
         error::{KeyRejected, Unspecified},
@@ -14,7 +14,7 @@ pub(in super::super) mod pkcs8 {
     // rounded to an even 64-bit words (4678 + 1% + padding ≈ 4728).
     const PKCS8_FIXED_CAPACITY_BUFFER: usize = 4728;
 
-    pub(in super::super) fn encode_v1_der(key: &LcPtr<EVP_PKEY>) -> Result<Vec<u8>, Unspecified> {
+    pub(in crate::rsa) fn encode_v1_der(key: &LcPtr<EVP_PKEY>) -> Result<Vec<u8>, Unspecified> {
         let mut buffer = vec![0u8; PKCS8_FIXED_CAPACITY_BUFFER];
         let out_len = {
             let mut cbb = LcCBB::new_fixed(<&mut [u8; PKCS8_FIXED_CAPACITY_BUFFER]>::try_from(
@@ -33,7 +33,7 @@ pub(in super::super) mod pkcs8 {
     }
 
     // Supports v1 and v2 encodings through a single API entry-point.
-    pub(in super::super) fn decode_der(pkcs8: &[u8]) -> Result<LcPtr<EVP_PKEY>, KeyRejected> {
+    pub(in crate::rsa) fn decode_der(pkcs8: &[u8]) -> Result<LcPtr<EVP_PKEY>, KeyRejected> {
         LcPtr::try_from(pkcs8)
     }
 }
@@ -41,10 +41,10 @@ pub(in super::super) mod pkcs8 {
 /// [RFC 8017](https://www.rfc-editor.org/rfc/rfc8017.html)
 ///
 /// PKCS #1: RSA Cryptography Specifications Version 2.2
-pub(in super::super) mod rfc8017 {
+pub(in crate::rsa) mod rfc8017 {
     use crate::{
         cbs,
-        error::Unspecified,
+        error::{KeyRejected, Unspecified},
         ptr::{DetachableLcPtr, LcPtr},
     };
     use aws_lc::{
@@ -54,7 +54,7 @@ pub(in super::super) mod rfc8017 {
     use std::ptr::null_mut;
 
     /// DER encode a RSA public key to `RSAPublicKey` structure.
-    pub(in super::super) unsafe fn encode_public_key_der(
+    pub(in crate::rsa) unsafe fn encode_public_key_der(
         pubkey: &LcPtr<EVP_PKEY>,
     ) -> Result<Box<[u8]>, ()> {
         let mut pubkey_bytes = null_mut::<u8>();
@@ -74,9 +74,9 @@ pub(in super::super) mod rfc8017 {
 
     /// Decode a DER encoded `RSAPublicKey` structure.
     #[inline]
-    pub(in super::super) fn decode_public_key_der(
+    pub(in crate::rsa) fn decode_public_key_der(
         public_key: &[u8],
-    ) -> Result<LcPtr<EVP_PKEY>, Unspecified> {
+    ) -> Result<LcPtr<EVP_PKEY>, KeyRejected> {
         let mut cbs = unsafe { cbs::build_CBS(public_key) };
 
         let rsa = DetachableLcPtr::new(unsafe { RSA_parse_public_key(&mut cbs) })?;
@@ -84,7 +84,7 @@ pub(in super::super) mod rfc8017 {
         let pkey = LcPtr::new(unsafe { EVP_PKEY_new() })?;
 
         if 1 != unsafe { EVP_PKEY_assign_RSA(*pkey, *rsa) } {
-            return Err(Unspecified);
+            return Err(KeyRejected::unspecified());
         }
 
         rsa.detach();
@@ -94,7 +94,7 @@ pub(in super::super) mod rfc8017 {
 
     /// Decodes a DER encoded `RSAPrivateKey` structure.
     #[inline]
-    pub(in super::super) fn decode_private_key_der(
+    pub(in crate::rsa) fn decode_private_key_der(
         private_key: &[u8],
     ) -> Result<LcPtr<EVP_PKEY>, Unspecified> {
         let mut cbs = unsafe { cbs::build_CBS(private_key) };
@@ -116,11 +116,17 @@ pub(in super::super) mod rfc8017 {
 /// [RFC 5280](https://www.rfc-editor.org/rfc/rfc5280.html)
 ///
 /// Encodings that use the `SubjectPublicKeyInfo` structure.
-pub(in super::super) mod rfc5280 {
-    use crate::{cbb::LcCBB, cbs, encoding::PublicKeyX509Der, error::Unspecified, ptr::LcPtr};
+pub(in crate::rsa) mod rfc5280 {
+    use crate::{
+        cbb::LcCBB,
+        cbs,
+        encoding::PublicKeyX509Der,
+        error::{KeyRejected, Unspecified},
+        ptr::LcPtr,
+    };
     use aws_lc::{EVP_marshal_public_key, EVP_parse_public_key, EVP_PKEY};
 
-    pub(in super::super) fn encode_public_key_der(
+    pub(in crate::rsa) fn encode_public_key_der(
         key: &LcPtr<EVP_PKEY>,
     ) -> Result<PublicKeyX509Der<'static>, Unspecified> {
         let mut der = LcCBB::new(1024);
@@ -132,9 +138,9 @@ pub(in super::super) mod rfc5280 {
         Ok(PublicKeyX509Der::from(der.into_buffer()?))
     }
 
-    pub(in super::super) fn decode_public_key_der(
+    pub(in crate::rsa) fn decode_public_key_der(
         value: &[u8],
-    ) -> Result<LcPtr<EVP_PKEY>, Unspecified> {
+    ) -> Result<LcPtr<EVP_PKEY>, KeyRejected> {
         let mut der = unsafe { cbs::build_CBS(value) };
         Ok(LcPtr::new(unsafe { EVP_parse_public_key(&mut der) })?)
     }
