6e278e14d7e0d9026b9b5889a6b9b40822058936
aws-lc-fips-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2024-04-02 20:42:30 +0000

    Generated headers

--- aws-lc-fips-sys/generated-include/openssl/boringssl_prefix_symbols.h
+++ aws-lc-fips-sys/generated-include/openssl/boringssl_prefix_symbols.h
@@ -17,7 +17,7 @@
 #define BORINGSSL_PREFIX_SYMBOLS_H	
 
 #ifndef BORINGSSL_PREFIX
-#define BORINGSSL_PREFIX aws_lc_fips_0_12_6
+#define BORINGSSL_PREFIX aws_lc_fips_0_12_7
 #endif // BORINGSSL_PREFIX
 
 
--- aws-lc-fips-sys/generated-include/openssl/boringssl_prefix_symbols_asm.h
+++ aws-lc-fips-sys/generated-include/openssl/boringssl_prefix_symbols_asm.h
@@ -20,7 +20,7 @@
 #define BORINGSSL_PREFIX_SYMBOLS_ASM_H
 
 #ifndef BORINGSSL_PREFIX
-#define BORINGSSL_PREFIX aws_lc_fips_0_12_6
+#define BORINGSSL_PREFIX aws_lc_fips_0_12_7
 #endif // BORINGSSL_PREFIX
 
 // On iOS and macOS, we need to treat assembly symbols differently from other
