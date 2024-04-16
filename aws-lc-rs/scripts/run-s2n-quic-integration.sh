b1bd0e2dd1e311d10eca2881856a8fcee5fd7580
Justin Smith <justsmth@amazon.com>
2024-04-16 10:36:19 -0400

    Update s2n-quic integration test

--- aws-lc-rs/scripts/run-s2n-quic-integration.sh
+++ aws-lc-rs/scripts/run-s2n-quic-integration.sh
@@ -2,19 +2,16 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0 OR ISC
 
-ROOT=$PWD
-QUIC_RING_STRING="^ring = { .* optional .* }"
-QUIC_OUR_STRING="ring = { path = \"${PWD}\", optional = true, package = \"aws-lc-rs\"}"
-QUIC_CRYPTO_RING_STRING="^ring = { .* }"
-QUIC_CRYPTO_OUR_STRING="ring = { path = \"${PWD}\", package = \"aws-lc-rs\"}"
+QUIC_AWS_LC_RS_STRING="^aws-lc-rs = { .* }"
+QUIC_PATH_STRING="aws-lc-rs = { path = \"${PWD}\" }"
 
 git clone https://github.com/aws/s2n-quic.git
 cd s2n-quic
 
 # replace instances of ring with our crate
 if [[ "$(uname)" == "Darwin" ]]; then
-	find ./ -type f  -name "Cargo.toml" | xargs sed -i '' -e "s|${QUIC_RING_STRING}|${QUIC_OUR_STRING}|g" -e "s|${QUIC_CRYPTO_RING_STRING}|${QUIC_CRYPTO_OUR_STRING}|g"
+	find ./ -type f  -name "Cargo.toml" | xargs sed -i '' -e "s|${QUIC_AWS_LC_RS_STRING}|${QUIC_PATH_STRING}|"
 else
-	find ./ -type f  -name "Cargo.toml" | xargs sed -i -e "s|${QUIC_RING_STRING}|${QUIC_OUR_STRING}|g" -e "s|${QUIC_CRYPTO_RING_STRING}|${QUIC_CRYPTO_OUR_STRING}|g"
+	find ./ -type f  -name "Cargo.toml" | xargs sed -i -e "s|${QUIC_AWS_LC_RS_STRING}|${QUIC_PATH_STRING}|"
 fi
 cargo test
