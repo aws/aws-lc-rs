01243a4273e0c6c3f18759bcce2141e8f6b9e0cc
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2024-05-02 21:07:52 +0000

    Collected source files for x86_64-unknown-linux-musl

--- aws-lc-sys/builder/cc_builder/x86_64_unknown_linux_musl.rs
+++ aws-lc-sys/builder/cc_builder/x86_64_unknown_linux_musl.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Tue Apr 23 13:33:04 UTC 2024
+// Thu May  2 21:06:45 UTC 2024
 
 use crate::cc_builder::Library;
 
@@ -85,6 +85,7 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "crypto/decrepit/cfb/cfb.c",
         "crypto/decrepit/dh/dh_decrepit.c",
         "crypto/decrepit/evp/evp_do_all.c",
+        "crypto/decrepit/obj/obj_decrepit.c",
         "crypto/decrepit/ripemd/ripemd.c",
         "crypto/decrepit/rsa/rsa_decrepit.c",
         "crypto/decrepit/x509/x509_decrepit.c",
@@ -132,6 +133,8 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "crypto/kyber/pqcrystals_kyber_ref_common/fips202.c",
         "crypto/lhash/lhash.c",
         "crypto/mem.c",
+        "crypto/ml_kem/ml_kem.c",
+        "crypto/ml_kem/ml_kem_512_ipd.c",
         "crypto/obj/obj.c",
         "crypto/obj/obj_xref.c",
         "crypto/ocsp/ocsp_asn.c",
