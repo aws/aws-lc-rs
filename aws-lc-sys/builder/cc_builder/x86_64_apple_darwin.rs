acb5a0ba91c14693caef67991c1893fffd0098f2
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2024-05-15 18:53:42 +0000

    Collected source files from macos-12

--- aws-lc-sys/builder/cc_builder/x86_64_apple_darwin.rs
+++ aws-lc-sys/builder/cc_builder/x86_64_apple_darwin.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Thu May  2 21:08:58 UTC 2024
+// Wed May 15 18:52:37 UTC 2024
 
 use crate::cc_builder::Library;
 
