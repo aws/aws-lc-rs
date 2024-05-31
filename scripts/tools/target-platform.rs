ebe9b6ea8b5ffb0859c8c9c601c4653ae1db693f
Justin W Smith <103147162+justsmth@users.noreply.github.com>
2024-05-31 16:09:38 -0400

    Pin rust-scripts (#433)

--- scripts/tools/target-platform.rs
+++ scripts/tools/target-platform.rs
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S cargo +nightly -Zscript
+#!/usr/bin/env -S cargo +nightly-2024-05-22 -Zscript
 
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
