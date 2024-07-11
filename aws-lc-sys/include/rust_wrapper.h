7f20361e4a49c72531cbdd401b85ecf726d3a4a2
Justin Smith <justsmth@amazon.com>
2024-07-11 11:18:05 -0400

    Add AWS-LC experimental kem header

--- aws-lc-sys/include/rust_wrapper.h
+++ aws-lc-sys/include/rust_wrapper.h
@@ -78,6 +78,7 @@
 #include "openssl/x509.h"
 #include "openssl/x509_vfy.h"
 #include "openssl/x509v3.h"
+#include "openssl/experimental/kem_deterministic_api.h"
 
 #if defined(AWS_LC_RUST_INCLUDE_SSL)
 #include "openssl/ssl.h"
