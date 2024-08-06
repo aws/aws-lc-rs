74b54da55c84028d80a39ca3d516300b7504d694
Justin Smith <justsmth@amazon.com>
2024-08-06 09:14:06 -0400

    PR-486

--- scripts/build/collect_symbols.sh
+++ scripts/build/collect_symbols.sh
@@ -57,23 +57,23 @@ if [[ ! -d "${AWS_LC_DIR}" ]]; then
 fi
 
 function filter_symbols() {
-  grep -v -E "^bignum_" | grep -v "curve25519_x25519" | grep -v "edwards25519_"
+  grep -E '^\w*$' | grep -v -E "^bignum_" | grep -v "curve25519_x25519" | grep -v "edwards25519_"
 }
 
 function filter_nm_symbols() {
-  grep -v -E '^_Z' | grep -v 'BORINGSSL_bcm_' | grep -v 'BORINGSSL_integrity_test'
+  grep -v -E '^_Z' | grep -v -E '^\?' | grep -v 'BORINGSSL_bcm_' | grep -v 'BORINGSSL_integrity_test'
 }
 
-function filter_macho_symbols() {
+function filter_windows_symbols() {
+   grep -v -E '^_*v?f?s?n?printf' | grep -v -E '^_*v?s?f?scanf' | grep -v RtlSecureZeroMemory | grep -v gai_strerrorA
+}
+
+function remove_leading_underscore() {
   grep -E '^_' | sed -e 's/^_\(.*\)/\1/'
 }
 
 function find_libcrypto() {
-  find "${REPO_ROOT}/target" -type f \( -name "lib*crypto.a" -o -name "lib*crypto.so" -o -name "lib*crypto.dylib" \) | grep "${CRATE_NAME}"
-}
-
-function find_libssl() {
-  find "${REPO_ROOT}/target" -type f \( -name "lib*ssl.a" -o -name "lib*ssl.so" -o -name "lib*ssl.dylib" \) | grep "${CRATE_NAME}"
+  find "${REPO_ROOT}/target" -type f \( -name "*crypto.lib" -o -name "lib*crypto.a" -o -name "lib*crypto.so" -o -name "lib*crypto.dylib" \) | grep "${CRATE_NAME}"
 }
 
 LIBCRYPTO_PATH="$(find_libcrypto)"
@@ -82,22 +82,37 @@ if [[ "${?}" -ne 0 ]]; then
   exit 1
 fi
 
-LIBSSL_PATH="$(find_libssl)"
-if [[ "${?}" -ne 0 ]]; then
-  echo "Unable to find libssl"
-  exit 1
-fi
-
 mkdir -p "$(dirname "${SYMBOLS_FILE}")"
 echo Writing symbols to: ${SYMBOLS_FILE}
 
-if [[ "${LIBCRYPTO_PATH}" = *.dylib ]]; then
-  nm --extern-only --defined-only -j  "${LIBCRYPTO_PATH}" "${LIBSSL_PATH}" | grep -v "${REPO_ROOT}" | sort | uniq | filter_macho_symbols | filter_nm_symbols |  filter_symbols >"${SYMBOLS_FILE}"
-elif [[ "${LIBCRYPTO_PATH}" = *.so ]]; then
-  nm --extern-only --defined-only --format=just-symbols  "${LIBCRYPTO_PATH}" "${LIBSSL_PATH}" | grep -v "${REPO_ROOT}" | sort | uniq | filter_nm_symbols | filter_symbols >"${SYMBOLS_FILE}"
+if [[ "${PLATFORM}" = *-msvc ]]; then
+  if [[ "${PLATFORM}" = aarch64-* ]]; then
+    MSVC_ARCH=arm64
+  elif [[ "${PLATFORM}" = i686-* ]]; then
+    MSVC_ARCH=x86
+  else
+    MSVC_ARCH=x64
+  fi
+  PFx86=$(printenv "ProgramFiles(x86)")
+  VS_INSTALL_PATH="$("$(echo "${PFx86//\\/\/}//Microsoft Visual Studio/Installer/vswhere.exe")" | grep 'resolvedInstallationPath:' | sed -e 's/[^:]*: \(.*\)$/\1/')"
+
+  DUMPBIN="$(ls -1 "${VS_INSTALL_PATH//\\/\/}"/VC/Tools/MSVC/*/bin/Hostx64/${MSVC_ARCH}/dumpbin.exe | tail -n 1)"
+  PATH="$(dirname "${DUMPBIN/C:/\/c}")":"${PATH}"
+  if [[ "${MSVC_ARCH}" = x64 ]]; then
+    dumpbin //EXPORTS //SYMBOLS  "${LIBCRYPTO_PATH}" | grep External | grep -v UNDEF | sed -e 's/.*External\s*|\s*\(.*\)$/\1/' | filter_windows_symbols | grep -E '^\w*$' | sort | uniq >"${SYMBOLS_FILE}"
+  elif [[ "${MSVC_ARCH}" = x86 ]]; then
+    dumpbin //EXPORTS //SYMBOLS  "${LIBCRYPTO_PATH}" | grep External | grep -v UNDEF | sed -e 's/.*External\s*|\s*\(.*\)$/\1/' | remove_leading_underscore | filter_windows_symbols | grep -E '^\w*$' | sort | uniq >"${SYMBOLS_FILE}"
+  else
+    dumpbin //EXPORTS //SYMBOLS  "${LIBCRYPTO_PATH}" | grep External | grep -v UNDEF | sed -e 's/.*External\s*|\s*\(.*\)$/\1/' | filter_windows_symbols | sort | uniq | filter_symbols>"${SYMBOLS_FILE}"
+  fi
+  echo "dumpbin pipes: ${PIPESTATUS[@]}"
+elif [[ "${LIBCRYPTO_PATH}" = *.dylib ]]; then
+  nm --extern-only --defined-only -j  "${LIBCRYPTO_PATH}" | grep -v "${REPO_ROOT}" | sort | uniq | remove_leading_underscore | filter_nm_symbols |  filter_symbols >"${SYMBOLS_FILE}"
+elif [[ "${LIBCRYPTO_PATH}" = *.so || "${LIBCRYPTO_PATH}" = *.lib ]]; then
+  nm --extern-only --defined-only --format=just-symbols  "${LIBCRYPTO_PATH}" | sort | uniq | filter_nm_symbols | filter_symbols >"${SYMBOLS_FILE}"
 else
   pushd "${AWS_LC_DIR}"
-  go run -mod readonly "${AWS_LC_DIR}"/util/read_symbols.go "${LIBCRYPTO_PATH}" "${LIBSSL_PATH}" | filter_symbols >"${SYMBOLS_FILE}"
+  go run -mod readonly "${AWS_LC_DIR}"/util/read_symbols.go "${LIBCRYPTO_PATH}" | filter_symbols >"${SYMBOLS_FILE}"
   popd
 fi
 
--- scripts/generate/_generate_bindings.sh
+++ scripts/generate/_generate_bindings.sh
@@ -44,7 +44,7 @@ cargo clean --target-dir "${TEMP_TARGET_DIR}"
 # Sets AWS_LC_SYS_INTERNAL_BINDGEN=1 which will cause the generation bindings for a specific platform. This feature
 # is only intended for internal use thus is not a cargo feature. Requires bindgen to be enabled. The internal_bindgen
 # config is enabled so that the final crates doesn't expect to find the dynamically generated bindings.rs
-env AWS_LC_SYS_INTERNAL_BINDGEN=1 AWS_LC_FIPS_SYS_INTERNAL_BINDGEN=1 cargo build --target-dir "${TEMP_TARGET_DIR}" --features bindgen,ssl
+env AWS_LC_SYS_INTERNAL_BINDGEN=1 AWS_LC_FIPS_SYS_INTERNAL_BINDGEN=1 cargo build --target-dir "${TEMP_TARGET_DIR}" --features bindgen
 cargo clean --target-dir "${TEMP_TARGET_DIR}"
 
 popd &>/dev/null # ${CRATE_DIR}
