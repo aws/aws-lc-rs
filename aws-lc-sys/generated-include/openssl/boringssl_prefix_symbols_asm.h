38534b82ca85ae0cf9b7b5e7760c6b34f03d8c2a
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2024-09-09 18:12:48 +0000

    Generated headers

--- aws-lc-sys/generated-include/openssl/boringssl_prefix_symbols_asm.h
+++ aws-lc-sys/generated-include/openssl/boringssl_prefix_symbols_asm.h
@@ -20,7 +20,7 @@
 #define BORINGSSL_PREFIX_SYMBOLS_ASM_H
 
 #ifndef BORINGSSL_PREFIX
-#define BORINGSSL_PREFIX aws_lc_0_21_0
+#define BORINGSSL_PREFIX aws_lc_0_21_1
 #endif // BORINGSSL_PREFIX
 
 // On iOS and macOS, we need to treat assembly symbols differently from other
