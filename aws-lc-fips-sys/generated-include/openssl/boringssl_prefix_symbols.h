439d0926c2f3eedce582e54cfd19de6877eabd80
aws-lc-fips-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2024-11-22 14:13:49 +0000

    Generated headers

--- aws-lc-fips-sys/generated-include/openssl/boringssl_prefix_symbols.h
+++ aws-lc-fips-sys/generated-include/openssl/boringssl_prefix_symbols.h
@@ -17,7 +17,7 @@
 #define BORINGSSL_PREFIX_SYMBOLS_H	
 
 #ifndef BORINGSSL_PREFIX
-#define BORINGSSL_PREFIX aws_lc_fips_0_12_14
+#define BORINGSSL_PREFIX aws_lc_fips_0_12_15
 #endif // BORINGSSL_PREFIX
 
 
