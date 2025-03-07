741f80b5433dfb8c51296c674618241badbfeadb
Justin Smith <justsmth@amazon.com>
2025-03-07 11:13:25 -0500

    Also update aws-lc-fips-sys CMakeLists.txt

--- .github/docker_images/cmake_build_versions/aws_lc_rs_build.sh
+++ .github/docker_images/cmake_build_versions/aws_lc_rs_build.sh
@@ -6,6 +6,11 @@
 set -ex -o pipefail
 
 echo "Building with CMake Version: $(cmake --version)"
+FIPS_BUILD=1
+if cmake --version | grep -q " 4."; then
+    echo "CMake version is 4.x, skipping FIPS build"
+    FIPS_BUILD=0
+fi
 
 . "${HOME}/.cargo/env"
 SRC_DIR="${SRC_DIR:-/aws_lc_rs}"
@@ -15,11 +20,16 @@ pushd "${SRC_DIR}"
 cargo clean
 cargo test -p aws-lc-rs --features=unstable
 cargo clean
-cargo test -p aws-lc-rs --features=unstable,fips
-cargo clean
+if  [ ${FIPS_BUILD} -eq 1 ]; then
+  cargo test -p aws-lc-rs --features=unstable,fips
+  cargo clean
+fi
 cargo test -p aws-lc-rs --release --features=unstable
 cargo clean
-cargo test -p aws-lc-rs --release --features=unstable,fips
+if  [ ${FIPS_BUILD} -eq 1 ]; then
+  cargo test -p aws-lc-rs --release --features=unstable,fips
+  cargo clean
+fi
 
 
 popd # ${BUILD_DIR}
