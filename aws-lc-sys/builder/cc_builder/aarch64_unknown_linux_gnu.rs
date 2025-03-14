9990c83f80fe67db00c5c4cc2b1732e2bac76d36
Sean McGrail <549813+skmcgrail@users.noreply.github.com>
2025-03-14 15:53:36 +0000

    Regenerate aws-lc-sys @ v1.48.3 (#734)
    * Update to aws-lc-sys to v1.48.3
    
    * Add cross-rs testing of ssl feature
    
    * Symbols from macos-14-xlarge
    
    * Symbols from ubuntu-latest
    
    * Symbols from  x86_64-pc-windows-msvc
    
    * Symbols for i686-unknown-linux-gnu
    
    * Symbols for aarch64-unknown-linux-gnu
    
    * Symbols for arm-unknown-linux-gnueabi
    
    * Symbols from  i686-pc-windows-msvc
    
    * Symbols for armv7-unknown-linux-gnueabihf
    
    * Symbols for powerpc-unknown-linux-gnu
    
    * Symbols for powerpc64le-unknown-linux-gnu
    
    * Symbols for aarch64-unknown-linux-musl
    
    * Symbols for x86_64-unknown-linux-musl
    
    * Symbols from  aarch64-pc-windows-msvc
    
    * Symbols from macos-13
    
    * Symbols from  x86_64-pc-windows-gnu
    
    * Generated headers
    
    * Generated bindings from ubuntu-latest
    
    * Generated bindings from macos-14-xlarge
    
    * Generated bindings from macos-13
    
    * Generated bindings for x86_64-pc-windows-gnu
    
    * Generated bindings for i686-unknown-linux-gnu
    
    * Generated bindings for aarch64-linux-android
    
    * Generated bindings for aarch64-unknown-linux-gnu
    
    * Generated bindings for i686-pc-windows-msvc
    
    * Generated bindings for riscv64gc-unknown-linux-gnu
    
    * Generated bindings for x86_64-unknown-linux-musl
    
    * Generated bindings for x86_64-pc-windows-msvc
    
    * Collected source files from macos-14-xlarge
    
    * Generated bindings for aarch64-pc-windows-msvc
    
    * Collected source files from ubuntu-latest
    
    * Generated bindings for aarch64-unknown-linux-musl
    
    * Collected source files from macos-13
    
    * Collected source files for x86_64-unknown-linux-musl
    
    * Collected source files for i686-unknown-linux-gnu
    
    * Collected source files for aarch64-unknown-linux-gnu
    
    * Collected source files for aarch64-unknown-linux-musl
    
    * Collected NASM files
    
    ---------
    
    Co-authored-by: aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>

--- aws-lc-sys/builder/cc_builder/aarch64_unknown_linux_gnu.rs
+++ aws-lc-sys/builder/cc_builder/aarch64_unknown_linux_gnu.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Mon Mar 10 21:31:21 UTC 2025
+// Fri Mar 14 00:21:55 UTC 2025
 
 use crate::cc_builder::Library;
 
