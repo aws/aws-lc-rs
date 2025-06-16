efbe06bc5a57b74f2d24c5994a4ea4a26e616984
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2025-06-16 20:17:39 +0000

    Collected source files from ubuntu-latest

--- aws-lc-sys/builder/cc_builder/x86_64_unknown_linux_gnu.rs
+++ aws-lc-sys/builder/cc_builder/x86_64_unknown_linux_gnu.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Thu May  8 11:23:05 UTC 2025
+// Mon Jun 16 20:16:17 UTC 2025
 
 use crate::cc_builder::Library;
 
@@ -41,6 +41,7 @@ pub(super) const CRYPTO_LIBRARY: Library = Library {
         "crypto/bio/bio.c",
         "crypto/bio/bio_mem.c",
         "crypto/bio/connect.c",
+        "crypto/bio/dgram.c",
         "crypto/bio/errno.c",
         "crypto/bio/fd.c",
         "crypto/bio/file.c",
