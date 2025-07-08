50488da2aba1f07f46e8760eb60a22fa7919839c
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2025-07-08 21:10:25 +0000

    Collected source files for aarch64-unknown-linux-gnu

--- aws-lc-sys/builder/cc_builder/aarch64_unknown_linux_gnu.rs
+++ aws-lc-sys/builder/cc_builder/aarch64_unknown_linux_gnu.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Wed Jun 18 19:22:08 UTC 2025
+// Tue Jul  8 21:08:44 UTC 2025
 
 use crate::cc_builder::Library;
 
