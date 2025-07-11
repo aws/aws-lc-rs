8bc40494973cb9adc8cd2b8441fbc823f76ad531
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2025-07-11 16:14:17 +0000

    Collected source files for riscv64gc-unknown-linux-gnu

--- aws-lc-sys/builder/cc_builder/riscv64gc_unknown_linux_gnu.rs
+++ aws-lc-sys/builder/cc_builder/riscv64gc_unknown_linux_gnu.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Tue Jul  8 21:08:55 UTC 2025
+// Fri Jul 11 15:58:38 UTC 2025
 
 use crate::cc_builder::Library;
 
