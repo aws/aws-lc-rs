661cd39bb6418d334d4d4f0ad2760fca277ba76a
Justin Smith <justsmth@amazon.com>
2025-08-18 12:20:08 -0400

    Add copyright statement

--- aws-lc-rs/src/signature/parsed_public_key_tests.rs
+++ aws-lc-rs/src/signature/parsed_public_key_tests.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR ISC
+
 use crate::digest::{digest, SHA256};
 use crate::rsa::KeySize;
 use crate::signature::{
