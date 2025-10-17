17808ea35b3a3f70ebc15d5e62c274c4b86041ce
Justin Smith <justsmth@amazon.com>
2025-10-17 16:41:56 -0400

    Script for win-x86

--- aws-lc-sys/builder/cc_builder/win_x86.rs
+++ aws-lc-sys/builder/cc_builder/win_x86.rs
@@ -1,18 +1,19 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
+// Fri Oct 17 20:41:37 UTC 2025
 
 pub(super) const CRYPTO_LIBRARY: &[&str] = &[
-    "generated-src/win-x86/crypto/test/trampoline-x86.asm",
     "generated-src/win-x86/crypto/chacha/chacha-x86.asm",
-    "generated-src/win-x86/crypto/fipsmodule/sha256-586.asm",
     "generated-src/win-x86/crypto/fipsmodule/aesni-x86.asm",
-    "generated-src/win-x86/crypto/fipsmodule/sha512-586.asm",
-    "generated-src/win-x86/crypto/fipsmodule/co-586.asm",
     "generated-src/win-x86/crypto/fipsmodule/bn-586.asm",
-    "generated-src/win-x86/crypto/fipsmodule/vpaes-x86.asm",
+    "generated-src/win-x86/crypto/fipsmodule/co-586.asm",
     "generated-src/win-x86/crypto/fipsmodule/ghash-ssse3-x86.asm",
     "generated-src/win-x86/crypto/fipsmodule/ghash-x86.asm",
     "generated-src/win-x86/crypto/fipsmodule/md5-586.asm",
-    "generated-src/win-x86/crypto/fipsmodule/x86-mont.asm",
     "generated-src/win-x86/crypto/fipsmodule/sha1-586.asm",
+    "generated-src/win-x86/crypto/fipsmodule/sha256-586.asm",
+    "generated-src/win-x86/crypto/fipsmodule/sha512-586.asm",
+    "generated-src/win-x86/crypto/fipsmodule/vpaes-x86.asm",
+    "generated-src/win-x86/crypto/fipsmodule/x86-mont.asm",
+    "generated-src/win-x86/crypto/test/trampoline-x86.asm",
 ];
