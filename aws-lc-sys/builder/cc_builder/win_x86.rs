e5dd40ea1ae328ad09a85ee03e97fa609bb03166
aws-lc-sys-bindings-generator <aws-lc-github-bot@amazon.com>
2025-10-22 16:17:42 +0000

    Collected source files

--- aws-lc-sys/builder/cc_builder/win_x86.rs
+++ aws-lc-sys/builder/cc_builder/win_x86.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR ISC
-// Tue Oct 21 11:29:19 UTC 2025
+// Wed Oct 22 16:17:39 UTC 2025
 
 pub(super) const CRYPTO_LIBRARY: &[&str] = &[
     "generated-src/win-x86/crypto/chacha/chacha-x86.asm",
